efed1db22ea55b66e7e50c3f0688e0739f87599e
sjaime97 <38027776+sjaime97@users.noreply.github.com>
2018-08-04 00:36:24 -0700

    Update AgendaView.java

--- AgendaView.java
+++ AgendaView.java
@@ -9,13 +9,16 @@ import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
-import javax.swing.JTextArea;
 
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+/**
+ * The view responsible for displaying all events in agenda format 
+ * When user clicks on agenda, the user will be able to type in a start date and end date 
+ * @Authors Christina Andrade and Samantha Jaime
+ */
 public class AgendaView implements ViewStrategy
 {
-
-	//private static String[] fullNameDays = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
-
 	GregorianCalendar c; 
 	DataModel d; 
 	static JTextArea area;
@@ -25,12 +28,21 @@ public class AgendaView implements ViewStrategy
 	private int height; 
 	private int width; 
 
+	/**
+	 * Constructs the agenda with the end dates and start dates
+	 * @param start the start date 
+	 * @param end the end date 
+	 */
 	public AgendaView(String start, String end)
 	{
 		startDate = start;
 		endDate = end; 
 	}
-
+	
+	@Override 
+	/**
+	 * The method implemented from the View strategy interface that will draw the grid for the agenda view 
+	 */
 	public void drawEventGrid(GregorianCalendar c, DataModel d, Graphics2D g2, Component container)
 	{
 		width = container.getWidth(); 
@@ -53,7 +65,7 @@ public class AgendaView implements ViewStrategy
 
 		while (temp.get(Calendar.MONTH) != (endMonth - 1) || temp.get(Calendar.DAY_OF_MONTH) != (endDay) || temp.get(Calendar.YEAR) != endYear)
 		{
-			eventsForThisAgenda.addAll(d.getEvents(temp.get(Calendar.MONTH) + 1, temp.get(Calendar.DAY_OF_MONTH), 
+			eventsForThisAgenda.addAll(d.getEvents(temp.get(Calendar.MONTH) + 1, temp.get(Calendar.DAY_OF_MONTH) + 1, 
 					temp.get(Calendar.YEAR))); 
 
 			temp.add(Calendar.DAY_OF_MONTH, 1);
@@ -62,47 +74,68 @@ public class AgendaView implements ViewStrategy
 		}
 
 
-		if (eventsForThisAgenda.size() == 0)
+		if (eventsForThisAgenda.size() == 0) //If there are no  events scheduled for the date range then show the message
 		{
-			g2.drawString("No Events scheduled in this time frame", 60, 50); 
+			Font message = new JLabel().getFont();
+			Font letters = new Font(message.toString(), Font.BOLD, 21);
+			FontRenderContext lettering = g2.getFontRenderContext();
+			g2.setFont(letters);
+
+			g2.drawString("                                      No events scheduled for this time frame.", 50, 135); 
 		}
 		else
 		{
 			
-			for (int j = 0; j < eventsForThisAgenda.size(); j++)
+			for (int j = 0; j < eventsForThisAgenda.size(); j++) //If there are events then get the events from those dates and print them out on each line 
 			{
 
+				//draws the lines between each event
 				g2.setColor(Color.LIGHT_GRAY);
 				Line2D.Double hourLine = new Line2D.Double(0, (j + 1) * 60, container.getWidth(), (j + 1) * 60); 
 				g2.draw(hourLine); 
 
+				//Gets the font and font style for each event calling the toString() method
 				String eventDetails; 
+				Font description = new JLabel().getFont();
+				Font time = new Font(description.toString(), Font.BOLD, 16);
+				g2.setFont(time);
+				
+				//Gets all event details and prints it on the screen according with the font style and dimensions stated above
 				Event e = eventsForThisAgenda.get(j); 
 				eventDetails = e.getDate() + "     "  +e.getStartTime() + "  -  " + e.getEndTime() + "  " + e.getName() + " "; 
 				g2.setColor(Color.BLACK);
 				g2.drawString(eventDetails, 60, ((j+1) * 60) - 10);
 
-				height = j*60; 
+				height = j * 60; 
 			}
 			
 
 		}
 	}
 
-
+	@Override
+	/**
+	 * Implemented method from the ViewStrategy interface
+	 * Draws the header of the agenda view
+	 */
 	public void drawHeader(GregorianCalendar c, Graphics2D g2, Component container) 
 	{
-		Font f = g2.getFont(); 
-		FontRenderContext context = g2.getFontRenderContext(); 
+		Font text = new JLabel().getFont();
+		Font f = new Font(text.toString(), Font.BOLD, 24);
+		FontRenderContext context = g2.getFontRenderContext();
+		g2.setFont(f);
+		
 		Rectangle2D bounds = f.getStringBounds("Agenda", context);
-
-		double x = (container.getWidth() - bounds.getWidth()) / 2;
-
-		g2.drawString("Agenda: " + startDate + " - " + endDate, (int) x, container.getHeight()/2); 
+		double x = (container.getWidth() - bounds.getWidth())/(3.5);
+		g2.drawString("      Agenda: " + startDate + " - " + endDate, (int) x, container.getHeight()/2); 
 	}
 
 	@Override
-	public Dimension getGridDimension() {
+	/**
+	 * Gets the dimensions for the grid 
+	 */
+	public Dimension getGridDimension() 
+	{
 	
 		return new Dimension(width, height);
 	}
